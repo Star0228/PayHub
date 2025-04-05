b5ae755ce086876457b9ab25bcafe219757662b2
forsur <3992238160@qq.com>
2025-04-06 00:17:35 +0800

    backend framework

---
+++ src/main/java/com/payhub/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.payhub.config;
+
+import com.payhub.interceptors.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 登录 / 注册的前端请求路径接口不能拦截
+        registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login",
+                "/user/register", "/api/spider",
+        "/api/user/login", "api/user/register");
+    }
+}
